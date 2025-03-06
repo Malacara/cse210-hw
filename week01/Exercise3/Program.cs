fe04ec5679fc3f9c9041b6dcbad4e73862ea4efb
Malacara <elcopemcp@gmail.com>
2025-03-06 14:37:04 -0600

    Magic Number activity finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine();
+
+        Random magicNumber = new Random();
+        int number = magicNumber.Next(1, 100);
+        Console.WriteLine($"The magic number is {number}");
+
+        int pick = 0;
+        while (pick != number)
+        {
+            Console.Write("What is your guess? ");
+            pick = int.Parse(Console.ReadLine());
+
+            if (pick > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (pick < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("You guessed it!");
+
     }
 }
